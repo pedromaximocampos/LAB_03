281d2a15e1693ef1a4b69fbaa7816ebd9f971d9a
gabrielreisresende <gabriellrr2005@gmail.com>
2024-10-15 18:45:49 -0300

    estrutura inicial do projeto

---
+++ implementacao/api-sistema-moedas/src/test/java/com/sistemademoeda/api_sistema_moedas/ApiSistemaMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemademoeda.api_sistema_moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
